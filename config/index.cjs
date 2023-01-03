f69e43260472499ed5ca955f04daf99bdd3be122
huangchenxi <67139754+bin0927@users.noreply.github.com>
2023-01-03 10:57:31 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -25,15 +25,11 @@ const USER_CONFIG = {
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '9pFidoJF-q1JLVjLh8l_KPpzMVlxOh8RCZDI0W7Cncg',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '10-24',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
-        },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '*生日', name: '宝贝', year: '2002', date: '09-19',
         },
         {
           type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
